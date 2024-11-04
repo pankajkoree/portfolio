e5369ae4c9499c280131cb7a9be72a86483a8758
failedCoderrr <pankajkori2057@gmail.com>
2024-11-04 14:57:49 +0530

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 import React from "react";
-import github from "../../../public/github.png";
+import github from "../../../public/github.jpeg";
+import linkedin from "../../../public/linkedin.jpg";
+import youtube from "../../../public/youtube.webp";
+import sololearn from "../../../public/sololearn.png";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>
-      <Image src={github} alt="github" width={100} height={100} />
+    <div className="relative flex flex-col justify-center top-12 gap-4">
+      <div className="relative flex justify-center ">
+        <Image src={github} alt="github" className="w-[50px] h-[50px]" />
+        <Image src={linkedin} alt="linkedin" className="w-[50px] h-[50px]" />
+        <Image src={youtube} alt="youtube" className="w-[50px] h-[50px]" />
+        <Image src={sololearn} alt="sololearn" className="w-[50px] h-[50px]" />
+      </div>
+      <div className="relative flex justify-center ">
+        <p>
+          © 2024{" "}
+          <Link
+            className="underline hover:text-green-300"
+            href="https://github.com/pankajkoree"
+          >
+            Pankaj Kori
+          </Link>{" "}
+          . All rights reserved.
+        </p>
+      </div>
     </div>
   );
 };
