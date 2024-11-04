b4c04563977b6ee0393225b24a2e35b909a42588
failedCoderrr <pankajkori2057@gmail.com>
2024-11-04 15:07:35 +0530

    button

--- src/app/components/NavigationBar.jsx
+++ src/app/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavigationBar = () => {
   return (
-    <div className="relative flex justify-center top-4 text-gray-600 font-bold">
+    <div className="relative flex justify-center top-4 text-gray-600">
       <div className="relative flex bg-blue-200 p-2 rounded-full text-xl">
         <Button variant="nav">Home</Button>
         <Button variant="nav">Projects</Button>
