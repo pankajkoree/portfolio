8bc1092d4468c8fdf1a5eea356f418b3a6facaaf
failedCoderrr <pankajkori2057@gmail.com>
2024-11-04 15:18:54 +0530

    navigation bar

--- src/app/components/NavigationBar.jsx
+++ src/app/components/NavigationBar.jsx
@@ -1,20 +1,46 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NavigationBar = () => {
+  const router = useRouter();
+
+  const gotoHome = () => {
+    console.log("home page");
+    router.push("/");
+  };
+
+  const gotoProjects = () => {
+    console.log("projects page");
+    router.push("/projects");
+  };
+  const gotoAbout = () => {
+    console.log("about page");
+    router.push("/about");
+  };
+  const gotoContact = () => {
+    console.log("contact page");
+    router.push("/contact");
+  };
   return (
     <div className="relative flex justify-center top-4 text-gray-600">
       <div className="relative flex bg-blue-200 p-2 rounded-full text-xl">
-        <Button variant="nav" className="hover:scale-125">
+        <Button variant="nav" className="hover:scale-125" onClick={gotoHome}>
           Home
         </Button>
-        <Button variant="nav" className="hover:scale-125">
+        <Button
+          variant="nav"
+          className="hover:scale-125"
+          onClick={gotoProjects}
+        >
           Projects
         </Button>
-        <Button variant="nav" className="hover:scale-125">
+        <Button variant="nav" className="hover:scale-125" onClick={gotoAbout}>
           About
         </Button>
-        <Button variant="nav" className="hover:scale-125">
+        <Button variant="nav" className="hover:scale-125" onClick={gotoContact}>
           Contact
         </Button>
       </div>
