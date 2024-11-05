a43b153aaffbacf52081e1e4283b267a57dddbdc
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 20:38:48 +0530

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,11 @@ export default function Home() {
   const gotoLinkedIn = () => {
     router.push("https://www.linkedin.com/in/pankaj-koree/");
   };
+
   const gotoProjects = () => {
     router.push("/projects");
   };
+
   return (
     <div className="relative flex flex-col items-center mx-auto max-w-full xl:max-w-4xl xl:mt-4 text-center px-4">
       <Image
@@ -21,20 +23,20 @@ export default function Home() {
         className="h-[120px] xl:h-[250px] w-[120px] xl:w-[250px] rounded-full mt-6 xl:mt-8"
       />
 
-      <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-4">
+      <p className="text-base md:text-lg lg:text-xl xl:text-2xl mt-4">
         🟢 Available for new projects
       </p>
 
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold mt-4 font-serif">
+      <h2 className="text-xl md:text-3xl lg:text-3xl xl:text-4xl font-bold mt-4 font-serif">
         Crafting seamless, full-stack digital experiences with precision
       </h2>
 
-      <h3 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mt-2 max-w-3xl font-mono">
-        I&apos;m <span className="text-blue-600">Pankaj Kori</span> , a{" "}
+      <h3 className="text-lg md:text-2xl lg:text-3xl xl:text-3xl mt-2 max-w-3xl font-mono">
+        I&apos;m <span className="text-blue-600">Pankaj Kori</span>, a{" "}
         <span className="text-purple-600">full-stack developer</span> with a
         passion for building fast, scalable, and visually engaging web
         applications, combining front-end creativity with back-end efficiency to
-        deliver seamless user experiences
+        deliver seamless user experiences.
       </h3>
 
       <div className="relative flex flex-col xl:flex-row gap-4 m-4 xl:mt-8">
