5988f99d4ae6855e38335749af577f4aac8886f0
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 15:56:17 +0530

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
+"use client";
 import Image from "next/image";
 import coder from "../../public/coder.jpeg";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const gotoLinkedIn = () => {
+    router.push("https://www.linkedin.com/in/pankaj-koree/");
+  };
+  const gotoProjects = () => {
+    router.push("/projects");
+  };
   return (
     <div className="relative flex flex-col items-center mx-auto max-w-full xl:max-w-4xl xl:mt-4 text-center px-4">
       <Image
         src={coder}
         alt="cartoon profile"
-        className="h-[120px] xl:h-[200px] w-[120px] xl:w-[200px] rounded-full mt-8 xl:mt-8"
+        className="h-[120px] xl:h-[200px] w-[120px] xl:w-[200px] rounded-full mt-6 xl:mt-8"
       />
 
       <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl mt-4">
@@ -25,6 +36,15 @@ export default function Home() {
         applications, combining front-end creativity with back-end efficiency to
         deliver seamless user experiences
       </h3>
+
+      <div className="relative flex flex-col xl:flex-row gap-2 m-4">
+        <Button variant="outline" onClick={gotoProjects}>
+          Explore my work ⬇️
+        </Button>
+        <Button variant="outline" onClick={gotoLinkedIn}>
+          Let's connect
+        </Button>
+      </div>
     </div>
   );
 }
