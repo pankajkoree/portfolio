5270428ead86ac622b6787fff610f53f516bf0b3
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 19:30:17 +0530

    layout

--- src/app/components/NavigationBar.jsx
+++ src/app/components/NavigationBar.jsx
@@ -20,19 +20,36 @@ const NavigationBar = () => {
   const gotoContact = () => {
     router.push("/contact");
   };
+
   return (
-    <div className="relative flex justify-center top-2 text-gray-600">
-      <div className="relative flex bg-blue-200 p-1 xl:p-2 rounded-full">
-        <Button variant="nav" onClick={gotoHome}>
+    <div className="relative flex justify-center mt-4">
+      <div className="relative flex bg-gradient-to-r from-blue-400 to-blue-600 dark:from-gray-800 dark:to-gray-900 p-2 xl:p-3 rounded-full shadow-lg">
+        <Button
+          variant="nav"
+          onClick={gotoHome}
+          className="mx-2 px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+        >
           Home
         </Button>
-        <Button variant="nav" onClick={gotoProjects}>
+        <Button
+          variant="nav"
+          onClick={gotoProjects}
+          className="mx-2 px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+        >
           Projects
         </Button>
-        <Button variant="nav" onClick={gotoAbout}>
+        <Button
+          variant="nav"
+          onClick={gotoAbout}
+          className="mx-2 px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+        >
           About
         </Button>
-        <Button variant="nav" onClick={gotoContact}>
+        <Button
+          variant="nav"
+          onClick={gotoContact}
+          className="mx-2 px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+        >
           Contact
         </Button>
       </div>
