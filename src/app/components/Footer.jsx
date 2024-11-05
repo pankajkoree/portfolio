18596a5c4c704230fd6d7ffa6e5b01ea96dbf245
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 20:18:13 +0530

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import React from "react";
-import github from "../../../public/github.jpeg";
-import linkedin from "../../../public/linkedin.jpg";
-import youtube from "../../../public/youtube.webp";
-import sololearn from "../../../public/sololearn.png";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -29,38 +23,42 @@ const Footer = () => {
   };
 
   return (
-    <div className="relative flex flex-col justify-center top-2 xl:top-4 gap-2 xl:gap-4 text-gray-600 text-[16px] xl:text-xl">
-      <div className="relative flex justify-center ">
-        <Image
-          src={github}
-          alt="github"
-          className="w-[36px] xl:w-[50px] h-[36px] xl:h-[50px] hover:cursor-pointer"
+    <div className="relative flex flex-col justify-center top-2 xl:top-4 gap-2 xl:gap-4 text-gray-600 dark:text-gray-400 text-[16px] xl:text-xl transition-colors duration-300">
+      <div className="relative flex justify-center gap-3 xl:gap-6">
+        <p
+          className="hover:cursor-pointer hover:text-blue-500 hover:underline dark:hover:text-blue-400 "
           onClick={gotoGithub}
-        />
-        <Image
-          src={linkedin}
-          alt="linkedin"
-          className="w-[36px] xl:w-[50px] h-[36px] xl:h-[50px] hover:cursor-pointer"
+        >
+          Github ↗
+        </p>
+
+        <p
+          className="hover:cursor-pointer hover:text-blue-500 hover:underline dark:hover:text-blue-400 "
           onClick={gotoLinkedIn}
-        />
-        <Image
-          src={youtube}
-          alt="youtube"
-          className="w-[36px] xl:w-[50px] h-[36px] xl:h-[50px] hover:cursor-pointer"
+        >
+          Linkedin ↗
+        </p>
+
+        <p
+          className="hover:cursor-pointer hover:text-blue-500 hover:underline dark:hover:text-blue-400 "
           onClick={gotoYoutube}
-        />
-        <Image
-          src={sololearn}
-          alt="sololearn"
-          className="w-[36px] xl:w-[50px] h-[36px] xl:h-[50px] hover:cursor-pointer"
+        >
+          Youtube ↗
+        </p>
+
+        <p
+          className="hover:cursor-pointer hover:text-blue-500 hover:underline dark:hover:text-blue-400 "
           onClick={gotoSololearn}
-        />
+        >
+          SoloLearn ↗
+        </p>
       </div>
-      <div className="relative flex justify-center ">
+
+      <div className="relative flex justify-center">
         <p>
           © 2024{" "}
           <Link
-            className="underline hover:text-blue-500"
+            className="underline hover:text-blue-500 dark:hover:text-blue-400"
             href="https://github.com/pankajkoree"
           >
             Pankaj Kori
