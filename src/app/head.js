6839a99fd9677be24b16ccf25868077cb5d02853
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 20:03:03 +0530

    head

--- src/app/head.js
+++ src/app/head.js
@@ -1,11 +1,8 @@
 export default function Head() {
   return (
     <>
-      <title>Your Website Title</title>
-      <meta
-        name="description"
-        content="This is the description of your website."
-      />
+      <title>pankajkoree</title>
+      <meta name="pankajkoree's portfolio" content="portfolio" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
