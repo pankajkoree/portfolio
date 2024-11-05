14fb2e48b7377d27d4a213d72f8716e42792144e
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 21:30:24 +0530

    navigation bar

--- src/app/components/NavigationBar.jsx
+++ src/app/components/NavigationBar.jsx
@@ -38,14 +38,14 @@ const NavigationBar = () => {
       {/* Mobile Menu Icon */}
       <div className="flex justify-start w-full md:hidden">
         <button onClick={toggleMenu} className="p-2 text-black dark:text-white">
-          {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}{" "}
+          {menuOpen ? <FiX size={24} /> : <FiMenu size={24} />}
           {/* Toggle icon */}
         </button>
       </div>
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="flex flex-col bg-gradient-to-r from-blue-400 to-blue-600 dark:from-gray-800 dark:to-gray-900 p-4 rounded-lg shadow-lg absolute top-12 left-0 w-40 z-50">
+        <div className="flex flex-col bg-gradient-to-r from-blue-400 to-blue-600 dark:from-gray-500 dark:to-gray-800 p-4 rounded-lg shadow-lg absolute top-12 left-0 w-40 z-50">
           {/* Align items to the left */}
           <Button
             variant="nav"
@@ -79,35 +79,35 @@ const NavigationBar = () => {
       )}
 
       {/* Desktop Navigation */}
-      <div className="hidden md:flex justify-center bg-gradient-to-r from-blue-400 to-blue-600 dark:from-gray-800 dark:to-gray-900 p-2 xl:p-3 rounded-full shadow-lg mx-auto">
+      <div className="hidden md:flex justify-center bg-gradient-to-r from-blue-400 to-blue-600 dark:from-gray-500 dark:to-gray-800 p-2 xl:p-3 rounded-full shadow-lg mx-auto">
         {/* Centered with auto margins */}
         <div className="flex space-x-4 w-full justify-center">
           {/* Flex for equal spacing */}
           <Button
             variant="nav"
             onClick={gotoHome}
-            className="px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+            className="px-4 py-2 text-white transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
           >
             Home
           </Button>
           <Button
             variant="nav"
             onClick={gotoProjects}
-            className="px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+            className="px-4 py-2 text-white  transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
           >
             Projects
           </Button>
           <Button
             variant="nav"
             onClick={gotoAbout}
-            className="px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+            className="px-4 py-2 text-white  transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
           >
             About
           </Button>
           <Button
             variant="nav"
             onClick={gotoContact}
-            className="px-4 py-2 text-white dark:text-gray-300 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
+            className="px-4 py-2 text-white  transition duration-300 ease-in-out transform hover:scale-105 hover:bg-blue-700 dark:hover:bg-gray-700 rounded-full"
           >
             Contact
           </Button>
