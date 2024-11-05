00916c1c18b9610622ff94128de31d15f25607ae
failedCoderrr <pankajkori2057@gmail.com>
2024-11-05 21:39:40 +0530

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,16 +2,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import React from "react";
 
 const Contact = () => {
   return (
     <div className="px-4 sm:px-6 md:px-8 mt-4">
-      <div className="relative flex flex-col xl:p-4 xl:mx-auto justify-center border border-blue-200 xl:hover:shadow-md xl:hover:shadow-green-300  max-w-full xl:max-w-4xl rounded-xl p-4 xl:m-16 text-gray-700">
+      <div className="relative flex flex-col xl:p-4 xl:mx-auto justify-center border border-blue-200 xl:hover:shadow-md xl:hover:shadow-green-300  max-w-full xl:max-w-4xl rounded-xl p-4 xl:m-16 text-gray-700 dark:text-white">
         <h2 className="text-xl xl:text-2xl font-bold text-center mb-4 sm:mb-6">
           Contact Me
         </h2>
-        <p className="text-center text-gray-600 mb-2 sm:mb-8">
+        <p className="text-center text-gray-600 dark:text-white mb-2 sm:mb-8">
           Feel free to reach out if you have any questions or would like to
           collaborate!
         </p>
