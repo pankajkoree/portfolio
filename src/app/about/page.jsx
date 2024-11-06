c1c60adb61b04703f458ae47a2c190f211949508
failedCoderrr <pankajkori2057@gmail.com>
2024-11-06 15:31:11 +0530

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,6 +10,11 @@ import node from "../../../public/node.png";
 import express from "../../../public/express.png";
 import mongodb from "../../../public/database.png";
 import next from "../../../public/next.png";
+import vscode from "../../../public/vscode.png";
+import git from "../../../public/git.png";
+import postman from "../../../public/postman.png";
+import shadcn from "../../../public/shadcn.jpg";
+import npm from "../../../public/npm.png";
 
 const About = () => {
   return (
@@ -147,83 +152,44 @@ const About = () => {
       </div>
       {/* end of tech skills */}
 
-      <div className="relative flex flex-col md:flex-row gap-4">
-        {/* front-end */}
+      <div className="relative flex flex-col md:flex-row gap-4]">
+        {/* tools and software */}
         <div className="relative border border-blue-300 p-3 rounded-xl w-full xl:w-[50%]">
           <div>
-            <h1 className="text-xl font-bold">✨Frontend</h1>
+            <h1 className="text-xl font-bold">✨Tools & Software</h1>
             <p className="text-lg mb-6">
-              Building responsive and modern interfaces
+              Utilizing tools and software to enhance productivity
             </p>
           </div>
 
           <div className="relative flex gap-2 flex-wrap">
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[120px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={html} alt="html" className="w-[20px] h-[20px]" />
-              <p>HTML</p>
+              <Image src={vscode} alt="vscode" className="w-[20px] h-[20px]" />
+              <p>VS code</p>
             </div>
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[100px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={css} alt="css" className="w-[20px] h-[20px]" />
-              <p>CSS</p>
-            </div>
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={js} alt="js" className="w-[20px] h-[20px]" />
-              <p>JavaScript</p>
-            </div>
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={react} alt="react" className="w-[20px] h-[20px]" />
-              <p>React JS</p>
+              <Image src={git} alt="git" className="w-[20px] h-[20px]" />
+              <p>Git</p>
             </div>
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
               <Image
-                src={tailwind}
-                alt="tailwind"
+                src={postman}
+                alt="postman"
                 className="w-[20px] h-[20px]"
               />
-              <p>Tailwind</p>
+              <p>Postman</p>
+            </div>
+            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
+              <Image src={shadcn} alt="shadcn" className="w-[20px] h-[20px]" />
+              <p>Shadcn UI</p>
+            </div>
+            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[100px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
+              <Image src={npm} alt="npm" className="w-[20px] h-[20px]" />
+              <p>NPM</p>
             </div>
           </div>
         </div>
-        {/* end of front-end */}
-
-        {/* start of backend */}
-        <div className="relative border border-blue-300 p-3 rounded-xl w-full xl:w-[50%]">
-          {/* Add your backend skills content here */}
-          <div>
-            <h1 className="text-xl font-bold">✨Backend</h1>
-            <p className="text-lg mb-6">
-              Creating robust server-side applications
-            </p>
-          </div>
-
-          <div className="relative flex gap-2 flex-wrap">
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[135px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={node} alt="node" className="w-[20px] h-[20px]" />
-              <p>Node JS</p>
-            </div>
-
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image
-                src={express}
-                alt="express"
-                className="w-[20px] h-[20px]"
-              />
-              <p>Express JS</p>
-            </div>
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image src={next} alt="next" className="w-[20px] h-[20px]" />
-              <p>Next JS</p>
-            </div>
-            <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[160px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
-              <Image
-                src={mongodb}
-                alt="mongodb"
-                className="w-[20px] h-[20px]"
-              />
-              <p>Mongo DB</p>
-            </div>
-          </div>
-        </div>
+        {/* end of tools and software */}
       </div>
     </div>
   );
