3f01a69f9d209073f96c5e9fcf1b0f6d10dcc39a
pankajkoree <pankajkori2057@gmail.com>
2025-01-29 21:57:34 +0530

    react

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -98,7 +98,7 @@ const About = () => {
             </div>
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
               <Image src={react} alt="react" className="w-[20px] h-[20px]" />
-              <p>React JS</p>
+              <a href="https://react.dev/" target="_blank">React JS</a>
             </div>
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[150px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
               <Image
