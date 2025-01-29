e95a5a6d75f325ad3120c00b1fcc41ffaa4de6d3
pankajkoree <pankajkori2057@gmail.com>
2025-01-29 22:08:55 +0530

    mongo db

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -166,7 +166,7 @@ const About = () => {
                 alt="mongodb"
                 className="w-[20px] h-[20px]"
               />
-              <p>Mongo DB</p>
+              <a href="https://www.mongodb.com/" target="_blank">Mongo DB</a>
             </div>
             <div className="relative flex justify-center items-center gap-1 border-2 border-blue-400 p-2 w-[160px] rounded-full dark:hover:bg-gray-700 hover:bg-blue-200 dark:hover:border-white">
               <Image src={mysql} alt="mysql" className="w-[20px] h-[20px]" />
